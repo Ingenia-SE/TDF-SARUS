cf0b6c2ca7d3632c88e11ca83be7af7bb8c18719
Rodrigo Sánchez <62879165+rsanchezm98@users.noreply.github.com>
2021-02-09 10:25:20 +0100

    Context Diagram added
    Context Diagram Included

---
+++ SARUS_Project/DefaultComponent/DefaultConfig/ContextModeling.cpp
@@ -0,0 +1,31 @@
+/********************************************************************
+	Rhapsody	: 9.0.1 
+	Login		: RSM
+	Component	: DefaultComponent 
+	Configuration 	: DefaultConfig
+	Model Element	: ContextModeling
+//!	Generated Date	: Tue, 9, Feb 2021  
+	File Path	: DefaultComponent\DefaultConfig\ContextModeling.cpp
+*********************************************************************/
+
+//## auto_generated
+#include "ContextModeling.h"
+//## auto_generated
+#include "Climatology.h"
+//## auto_generated
+#include "Environment.h"
+//## auto_generated
+#include "Environment_Database.h"
+//## auto_generated
+#include "GPS_satellites.h"
+//## auto_generated
+#include "Legislation.h"
+//## auto_generated
+#include "SARUS_system.h"
+//## package BlockModeling::ContextModeling
+
+
+
+/*********************************************************************
+	File Path	: DefaultComponent\DefaultConfig\ContextModeling.cpp
+*********************************************************************/
---
+++ SARUS_Project/DefaultComponent/DefaultConfig/ContextModeling.h
@@ -0,0 +1,43 @@
+/*********************************************************************
+	Rhapsody	: 9.0.1 
+	Login		: RSM
+	Component	: DefaultComponent 
+	Configuration 	: DefaultConfig
+	Model Element	: ContextModeling
+//!	Generated Date	: Tue, 9, Feb 2021  
+	File Path	: DefaultComponent\DefaultConfig\ContextModeling.h
+*********************************************************************/
+
+#ifndef ContextModeling_H
+#define ContextModeling_H
+
+//## auto_generated
+#include <oxf\oxf.h>
+//## auto_generated
+#include <..\Profiles\SysML\SIDefinitions.h>
+//## auto_generated
+class Climatology;
+
+//## auto_generated
+class Environment;
+
+//## auto_generated
+class Environment_Database;
+
+//## auto_generated
+class GPS_satellites;
+
+//## auto_generated
+class Legislation;
+
+//## auto_generated
+class SARUS_system;
+
+//## package BlockModeling::ContextModeling
+
+
+
+#endif
+/*********************************************************************
+	File Path	: DefaultComponent\DefaultConfig\ContextModeling.h
+*********************************************************************/
