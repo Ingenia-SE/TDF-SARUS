fa0413a3812fc1d7a34333d40fdec7068e459844
APVGITHUB <alex.pr434@gmail.com>
2021-03-24 14:28:42 +0100

    added shell script

--- TDF-Vision/hector/launch.sh
+++ TDF-Vision/hector/launch.sh
@@ -1,9 +1,13 @@
+#!/bin/bash
+
+gnome-terminal  \
+--tab --title "gazebo_world" --command "bash -c \"
 source ./devel/setup.bash
-roslaunch hector_gazebo_worlds rescue_mission.launch &
-roslaunch hector_quadrotor_gazebo spawn_quadrotor_with_thermal.launch &
-rosrun image_view image_view image:=/thermal_cam/camera/image &
-sleep 20
-echo "enabling motors"
-rosservice call /enable_motors true
-rosrun teleop_twist_keyboard teleop_twist_keyboard.py
+roslaunch hector_gazebo_worlds rescue_mission.launch ;
+exec bash\"" \
+--tab --title "spawn" --command "bash -c \"
+sleep 5
+source ./devel/setup.bash 
+roslaunch hector_quadrotor_gazebo spawn_quadrotor_with_thermal.launch;
+exec bash\""
 
