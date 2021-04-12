5e630fd9ac6ff963cfd5a466c523ab719d1b1dad
jgsamartin <74541499+jgsamartin@users.noreply.github.com>
2021-04-12 22:51:17 +0200

    Update mission_setup.sh

--- TDF-MissionPlanning/mission_setup.sh
+++ TDF-MissionPlanning/mission_setup.sh
@@ -16,4 +16,9 @@ sudo apt install git -y
 sudo apt install python3-argcomplete -y
 sudo apt install aerostack-melodic -y
 
+# Python
+sudo apt install python3-pip
+sudo -H pip3 install --upgrade pip
+sudo pip3 install numpy geopy geopandas pandas tqdm shapely contextily matplotlib scikit-imag
+
 
